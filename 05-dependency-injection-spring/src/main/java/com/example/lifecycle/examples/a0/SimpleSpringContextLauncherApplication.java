8d6e3e6596d3648e04bd14e702572337af15e6c7
a-shekhar <rajadityasingh31@gmail.com>
2024-03-07 23:34:57 +0530

    Dependency Injection

---
+++ 05-dependency-injection-spring/src/main/java/com/example/lifecycle/examples/a0/SimpleSpringContextLauncherApplication.java
@@ -0,0 +1,16 @@
+package com.example.lifecycle.examples.a0;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+
+@Configuration
+@ComponentScan
+public class SimpleSpringContextLauncherApplication {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(SimpleSpringContextLauncherApplication.class);
+        Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out :: println);
+    }
+}
---
+++ 05-dependency-injection-spring/src/main/java/com/example/lifecycle/game/GamingAppLauncherApplication.java
@@ -0,0 +1,15 @@
+package com.example.lifecycle.game;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan
+public class GamingAppLauncherApplication {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(GamingAppLauncherApplication.class);
+        context.getBean(GamingConsole.class).up();
+        context.getBean(GameRunner.class).run();
+    }
+}
