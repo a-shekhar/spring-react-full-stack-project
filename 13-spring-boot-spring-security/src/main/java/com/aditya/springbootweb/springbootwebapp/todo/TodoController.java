ae230121c82fa18d63ee53afc3bad4fa0d2860bb
a-shekhar <rajadityasingh31@gmail.com>
2024-03-16 23:18:58 +0530

    Spring Security

---
+++ 13-spring-boot-spring-security/src/main/java/com/aditya/springbootweb/springbootwebapp/todo/TodoController.java
@@ -0,0 +1,89 @@
+package com.aditya.springbootweb.springbootwebapp.todo;
+
+import jakarta.validation.Valid;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Controller
+public class TodoController {
+
+    private TodoService todoService;
+
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    @RequestMapping("list-todos")
+    public String listAllTodos(ModelMap model){
+        String username = getLoggedInUserName();
+        List<Todo> todos = todoService.findByUserName(username);
+        model.addAttribute("todos", todos);
+        return "listTodos";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showNewTodoPage(ModelMap model) {
+        String username = getLoggedInUserName();
+        Todo todoObj = new Todo(0, username, "", LocalDate.now().plusYears(1), false);
+        model.addAttribute("todo", todoObj);
+        return "todo";
+    }
+
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodo(ModelMap model, @Valid Todo todo,  BindingResult result){
+        if(result.hasErrors()){
+            return "todo";
+        }
+        // redirect to this page
+        todoService.addTodo(getLoggedInUserName(), todo.getDescription(),
+               todo.getTargetDate(), false);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping("delete-todo")
+    public String deleteTodo(@RequestParam int id){
+        // delete to do
+        todoService.deleteById(id);
+        return "redirect:list-todos";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.GET)
+    public String showUpdateTodoPage(@RequestParam int id, ModelMap model){
+        // create binding
+        String username = getLoggedInUserName();
+        Todo todoObj = new Todo(0, username, "Default Desc", LocalDate.now().plusYears(1), false);
+        model.addAttribute("todo", todoObj);
+        // find the record
+        Todo todo = todoService.findById(id);
+        model.addAttribute("todo", todo);
+        return "todo";
+    }
+
+    @RequestMapping(value = "update-todo", method = RequestMethod.POST)
+    public String updateTodo(ModelMap model, @Valid Todo todo,  BindingResult result){
+        if(result.hasErrors()){
+            return "todo";
+        }
+        String username = getLoggedInUserName();
+        todo.setUsername(username);
+        // redirect to this page
+        todoService.updateTodo(todo);
+        return "redirect:list-todos";
+    }
+
+    private String getLoggedInUserName(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName();
+    }
+
+}
+
---
+++ 13-spring-boot-spring-security/src/main/java/com/aditya/springbootweb/springbootwebapp/todo/TodoService.java
@@ -0,0 +1,49 @@
+package com.aditya.springbootweb.springbootwebapp.todo;
+
+import jakarta.validation.Valid;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
+
+@Service
+public class TodoService {
+    private static List<Todo> todos = new ArrayList<>();
+    private  static int todosCount = 3;
+
+    static {
+        todos.add(new Todo(1, "Aditya", "Learn Spring Boot", LocalDate.of(2024, 8, 1), false));
+        todos.add(new Todo(2, "Raj", "Learn AWS", LocalDate.of(2024, 8, 1), false));
+        todos.add(new Todo(3, "Shekhar", "Learn GCP", LocalDate.of(2025, 1, 3), false));
+    }
+
+    public List<Todo> findByUserName(String userName) {
+        System.out.println("name" + userName);
+        Predicate<? super Todo> predicate = todo -> todo.getUsername().equalsIgnoreCase(userName);
+        return todos.stream().filter(predicate).toList();
+    }
+
+    public void addTodo(String username, String description, LocalDate targetDate, boolean isDone){
+        Todo todo = new Todo(++todosCount, username, description, targetDate, isDone);
+        todos.add(todo);
+    }
+
+    public void deleteById(int id) {
+        // if condition matches
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        todos.removeIf(predicate);
+    }
+
+    public Todo findById(int id) {
+        Predicate<? super Todo> predicate = todo -> todo.getId() == id;
+        Todo todo = todos.stream().filter(predicate).findFirst().get();
+        return todo;
+    }
+    public void updateTodo(@Valid Todo todo) {
+        deleteById(todo.getId());
+        todos.add(todo);
+    }
+}
