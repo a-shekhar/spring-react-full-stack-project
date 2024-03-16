91585f05222c5ce5011c05d2105ddc5e94b3a6c7
a-shekhar <rajadityasingh31@gmail.com>
2024-03-17 00:13:33 +0530

    Spring JPA with h2

---
+++ 14-spring-boot-jpa-database/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into todo (id, username, description, target_date, done) values(10001, 'aditya', 'Get AWS Certified', '2024-12-12', false);
+
+insert into todo (id, username, description, target_date, done) values(10002, 'aditya', 'Get GCP Certified', '2024-12-12', false);
+
+insert into todo (id, username, description, target_date, done) values(10003, 'aditya', 'Get Azure Certified', '2024-12-12', false);
