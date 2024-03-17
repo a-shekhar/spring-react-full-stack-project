5ee8f0a390eaccac783eddb01eb968e9168b9a46
a-shekhar <rajadityasingh31@gmail.com>
2024-03-17 20:24:50 +0530

    Post Method

---
+++ 15-spring-boot-rest-web-services-basics/src/main/java/com/springboot/springbootrestwebsericesbasics/user/UserResource.java
@@ -0,0 +1,38 @@
+package com.springboot.springbootrestwebsericesbasics.user;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findById(id);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        // location - users/4 => /users/{id}
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+        return ResponseEntity.created(location).build();
+    }
+
+
+}
