d5c05c34b1678942e3e8e06700779eab80e7f2d8
a-shekhar <rajadityasingh31@gmail.com>
2024-03-19 20:05:58 +0530

    Reset button

--- 19-counter-app/counter-app/src/components/Counter.jsx
+++ 19-counter-app/counter-app/src/components/Counter.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './Counter.css'
-import {PropTypes} from 'prop-types'
+import CounterButton from './CounterButton'
 
 export default function Counter(){
     const[count, setCount] = useState(0)
@@ -13,66 +13,19 @@ export default function Counter(){
         setCount(count - by)
     }
 
+    function resetCounter(){
+        setCount(0)
+    }
+
+
     return (
         <>
             <span className= "totalCount">{count}</span>
             <CounterButton by = {1} incrementMethod={incrementCounterParentFunction} decrementMethod = {decrementCounterParentFunction}/>
             <CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod = {decrementCounterParentFunction}/>
             <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod = {decrementCounterParentFunction}/>
+            <button className="resetButton" onClick={resetCounter}>Reset</button> 
         </>
     )
 }
 
-
-function CounterButton({by, incrementMethod, decrementMethod}){
-
-    // const buttonstyle = {
-    //     fontSize:"30px",
-    //     backgroundColor: "#00a5ab",
-    //     width : "100px",
-    //     margin : "10px",
-    //     color : "white",
-    //     padding : "15px",
-    //     borderRadius : "30px"
-    // }
-
-    // it returns an array  [0, ƒ] where o is initial value and f is function
-    //const state = useState(0);
-
-    const [count, setCount] = useState(0)
-
-    function incrementCounterFunction(){
-        //state[1](state[0] + 1)
-        setCount(count + by)
-        //console.log(state[0])
-        //console.log(state[1])
-        //console.log('increment clicked')
-        incrementMethod(by)
-        console.log(count)
-    }
-
-    function decrementCounterFunction(){
-        setCount(count - by)
-        decrementMethod(by)
-        console.log(count)
-    }
-
-    return (
-        <div className="counter">
-            {/* <span className= "count">{count}</span>  */}   
-            <div>
-
-                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
-                <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
-            </div>
-        </div>
-    )
-}
-
-CounterButton.propTypes = {
-    by: PropTypes.number 
-}
-
-CounterButton.defaultProps ={
-    by: 1
-}
---
+++ 19-counter-app/counter-app/src/components/CounterButton.jsx
@@ -0,0 +1,55 @@
+import {PropTypes} from 'prop-types'
+import { useState } from 'react'
+
+export default function CounterButton({by, incrementMethod, decrementMethod}){
+
+    // const buttonstyle = {
+    //     fontSize:"30px",
+    //     backgroundColor: "#00a5ab",
+    //     width : "100px",
+    //     margin : "10px",
+    //     color : "white",
+    //     padding : "15px",
+    //     borderRadius : "30px"
+    // }
+
+    // it returns an array  [0, ƒ] where o is initial value and f is function
+    //const state = useState(0);
+
+    const [count, setCount] = useState(0)
+
+    function incrementCounterFunction(){
+        //state[1](state[0] + 1)
+        setCount(count + by)
+        //console.log(state[0])
+        //console.log(state[1])
+        //console.log('increment clicked')
+        incrementMethod(by)
+        console.log(count)
+    }
+
+    function decrementCounterFunction(){
+        setCount(count - by)
+        decrementMethod(by)
+        console.log(count)
+    }
+
+    return (
+        <div className="counter">
+            {/* <span className= "count">{count}</span>  */}   
+            <div>
+
+                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
+                <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
+            </div>
+        </div>
+    )
+}
+
+CounterButton.propTypes = {
+    by: PropTypes.number 
+}
+
+CounterButton.defaultProps ={
+    by: 1
+}
