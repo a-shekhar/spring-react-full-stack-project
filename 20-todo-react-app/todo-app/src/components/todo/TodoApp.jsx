2037993e09a535b7ca705276d4a7ea02bf51b7c1
a-shekhar <rajadityasingh31@gmail.com>
2024-03-23 20:35:06 +0530

    Create todo's list

--- 20-todo-react-app/todo-app/src/components/todo/TodoApp.jsx
+++ 20-todo-react-app/todo-app/src/components/todo/TodoApp.jsx
@@ -9,7 +9,8 @@ export default function TodoApp(){
                 <Routes>
                     <Route path = '/' element ={<LoginComponent/>}></Route>
                     <Route path = '/login' element ={<LoginComponent/>}></Route>
-                    <Route path = '/welcome/:username' element ={<WelcomeComponent/>}></Route>
+                    {/* <Route path = '/welcome/:username' element ={<WelcomeComponent/>}></Route> */}
+                    <Route path = '/todos' element ={<ListTodosComponent/>}/>
                     <Route path = '/*' element ={<ErrorComponent/>}></Route>
                 </Routes>
             </BrowserRouter>
@@ -113,6 +114,44 @@ function ErrorComponent(){
 
 
 
+function ListTodosComponent(){
+    const today = new Date()
+    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
+
+    const todos = [{id : 1, description : 'Learn AWS', done : false, targetDate: targetDate},
+    {id : 2, description : 'Learn Azure',  done : false, targetDate: targetDate},
+    {id : 3, description : 'Learn GCP',  done : false, targetDate: targetDate}
+    ]
+    return(
+        <div className="ListTodosComponent">
+           <h1>Things you want to do!</h1>
+           <div>
+             <table>
+                <thead>
+                    <tr>
+                        <td>ID</td>
+                        <td>Description</td>
+                        <td>Is Done?</td>
+                        <td>Target date</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        todos.map(
+                            todo => (<tr key={todo.id}>
+                                <td>{todo.id}</td>
+                                <td>{todo.description}</td>
+                                <td>{todo.done.toString()}</td>
+                                <td>{todo.targetDate.toDateString()}</td>
+                            </tr>)
+                        )
+                    }
+                </tbody>
+             </table>
+           </div>
+        </div>
+    )
+}
 
 
 // function SuccessMessageComponent(){
