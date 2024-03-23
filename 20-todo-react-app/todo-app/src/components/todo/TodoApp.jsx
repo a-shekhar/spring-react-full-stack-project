c9b4db615765793b401f742f76bc11e4a35f9c6d
a-shekhar <rajadityasingh31@gmail.com>
2024-03-23 19:33:49 +0530

    Success and Error Message added

--- 20-todo-react-app/todo-app/src/components/todo/TodoApp.jsx
+++ 20-todo-react-app/todo-app/src/components/todo/TodoApp.jsx
@@ -17,20 +17,57 @@ function LoginComponent(){
 
     const [password, setPassword] =useState('')
 
+    const [showSuccessMessage, setShowSuccessMessage] =useState(false)
+
+    const [showErrorMessage, setShowErrorMessage] =useState(false)
+
     function handleUsernameChange(event){
-        console.log(event.target.value)
+        //console.log(event.target.value)
         setUsername(event.target.value)
     }
 
 
     function handlePasswordChange(event){
-        console.log(event.target.value)
+        //console.log(event.target.value)
         setPassword(event.target.value)
     }
 
+    function SuccessMessageComponent(){
+        if(showSuccessMessage){
+            return(<div className='successMessage'>Authentication successful</div>)
+        }
+        return null;
+    }
+
+    function ErrorMessageComponent(){
+        if(showErrorMessage){
+            return(<div className='errorMessage'>Authentication failed. Please check your credentials</div>)
+        }
+        return null;
+    }
+
+
+    function handleSubmit(){
+       // console.log(username);
+       // console.log(password)
+       if(username === "aditya" && password == "dummy"){
+            console.log('Success')
+            setShowSuccessMessage(true)
+            setShowErrorMessage(false)
+       }else{
+            console.log('Failed')
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+       }
+    }
+
+   
 
     return(
         <div className="Login">
+            <SuccessMessageComponent/>
+            <ErrorMessageComponent/>
+
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
@@ -41,7 +78,7 @@ function LoginComponent(){
                     <input type="password" name = "password" value = {password} onChange={handlePasswordChange}/>
                 </div>
                 <div>
-                    <button type="button" name = "login"> Login </button>
+                    <button type="button" name = "login" onClick={handleSubmit}> Login </button>
                 </div>
             </div>
         </div>
