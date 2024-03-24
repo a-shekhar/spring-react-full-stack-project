42615c95e5cdffe306dd922ee614d014854c800c
a-shekhar <rajadityasingh31@gmail.com>
2024-03-24 23:23:10 +0530

    Calling rest api from react

--- 21-spring-react-full-stack-basics/src/main/java/com/springboot/springreactfullstackbasics/SpringReactFullStackBasicsApplication.java
+++ 21-spring-react-full-stack-basics/src/main/java/com/springboot/springreactfullstackbasics/SpringReactFullStackBasicsApplication.java
@@ -2,6 +2,9 @@ package com.springboot.springreactfullstackbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SpringReactFullStackBasicsApplication {
@@ -10,4 +13,17 @@ public class SpringReactFullStackBasicsApplication {
 		SpringApplication.run(SpringReactFullStackBasicsApplication.class, args);
 	}
 
+	// cross-origin requests
+	// Allow all requests only from http://localhost:3000/
+	@Bean
+	public WebMvcConfigurer corsConfigurer(){
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedMethods("*")
+						.allowedOrigins("http://localhost:3000/");
+			}
+		};
+	}
 }
--- 22-todo-app-for-spring-boot-connection/src/components/todo/WelcomeComponent.jsx
+++ 22-todo-app-for-spring-boot-connection/src/components/todo/WelcomeComponent.jsx
@@ -1,9 +1,39 @@
 import {useParams, Link} from 'react-router-dom'
+import { useState } from 'react'
+import { retrieveHelloWorldBean } from './api/HelloWorldApiService'
 
 export default function WelcomeComponent(){
     const {username} = useParams()
    // console.log(params.username)
   // console.log(username)
+
+    const[message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi(){
+        console.log('called')
+        // axios to call rest api
+        // axios.get('http://localhost:8080/hello-world')
+        // .then((response) => successfulResponse(response))
+        // .catch( (error) => errorResponse(error))
+        // .finally( () => console.log('cleanup'))
+
+        retrieveHelloWorldBean()
+        .then((response) => successfulResponse(response))
+        .catch( (error) => errorResponse(error))
+        .finally( () => console.log('cleanup'))
+    }
+
+
+    function successfulResponse(response){
+        console.log(response)
+        //setMessage(response.data)
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error){
+        console.log(error)
+    }
+
     return(
     <div>
         <h1> Welcome to {username}'s world!</h1>
@@ -12,7 +42,11 @@ export default function WelcomeComponent(){
         </div>
 
         <div>
-            <button className='btn btn-success'>Call Hello World REST API</button>
+            <button className='btn btn-success m-5' onClick={callHelloWorldRestApi}>Call Hello World</button>
+        </div>
+
+        <div>
+            <div className='text-info'>{message}</div>
         </div>
 
         </div>
