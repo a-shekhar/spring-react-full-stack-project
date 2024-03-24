f6ae416295ab405e8482b5d630d28d3581513e4f
a-shekhar <rajadityasingh31@gmail.com>
2024-03-24 21:04:16 +0530

    Spring Boot react full stack init

---
+++ 21-spring-react-full-stack-basics/src/main/java/com/springboot/springreactfullstackbasics/SpringReactFullStackBasicsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.springreactfullstackbasics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringReactFullStackBasicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringReactFullStackBasicsApplication.class, args);
+	}
+
+}
---
+++ 21-spring-react-full-stack-basics/src/test/java/com/springboot/springreactfullstackbasics/SpringReactFullStackBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.springreactfullstackbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringReactFullStackBasicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
