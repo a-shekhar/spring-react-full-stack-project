5026ac612bdfaa225907d58dfa0b6282cdfc5bfc
a-shekhar <rajadityasingh31@gmail.com>
2024-03-28 15:20:33 +0530

    Update and validate course

--- 22-todo-app-for-spring-boot-connection/src/components/todo/TodoComponent.jsx
+++ 22-todo-app-for-spring-boot-connection/src/components/todo/TodoComponent.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom"
 import { retrieveTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext"
 import { useEffect, useState } from "react"
+import { Field, Formik, Form, ErrorMessage} from "formik"
 
 export default function TodoComponent(){
 
@@ -10,6 +11,7 @@ export default function TodoComponent(){
     const username = authContext.username
 
     const [description, setDescription] = useState('')
+    const [targetDate, setTargetDate] = useState('')
 
     useEffect(
         () => retrieveTodos(), [id] 
@@ -19,16 +21,73 @@ export default function TodoComponent(){
         retrieveTodoApi(username, id)
         .then(response => {
             setDescription(response.data.description)  
+            setTargetDate(response.data.targetDate)
         }
         )
         .catch(error => console.log(error))
+    }
+
+    function onSubmit(values){
+        console.log(values)
+    }
+
+    function validate(values){
+        let errors = {
+            // description: 'Enter a valid description',
+            // targetDate: 'Enter a valid target date'
+        }
+
+        if(values.description.length < 5){
+            errors.description = 'Enter at-least 5 characters'
+        }
+
+        if(values.targetDate == null){
+            errors.targetDate = 'Enter a target date'
+        }
+
+
+        console.log(values)
+        return errors
     }
 
     return (
         <div className="container">
             <h1>Enter todo Details</h1>
             <div>
-                 Description : {description}
+                <Formik initialValues={ {description, targetDate}} enableReinitialize={true} onSubmit={onSubmit}
+                validate={validate} validateOnChange = {false} validateOnBlur = {false}>
+                    {
+                        (props) => (
+                            <Form>
+                                <ErrorMessage 
+                                    name = "description"
+                                    component = "div"
+                                    className= "alert alert-warning"
+                                />
+
+                                <ErrorMessage 
+                                    name = "targetDate"
+                                    component = "div"
+                                    className= "alert alert-warning"
+                                />
+
+
+                                <fieldset className="form-group">
+                                    <label>Description</label>
+                                    <Field type="text" className="form-control" name ="description"/>
+                                </fieldset>
+                                
+                                <fieldset className="form-group">
+                                    <label>Target Date</label>
+                                    <Field type="date" className="form-control" name ="targetDate"/>
+                                </fieldset>
+                                <div>
+                                    <button className="btn btn-success m-5" type="submit">Save</button>
+                                </div>
+                            </Form>
+                        )
+                    }
+                </Formik>
             </div>
         </div>
     )
