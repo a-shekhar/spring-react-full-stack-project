168f11b5469002dcd173ba20234b8e2714fb8b5e
a-shekhar <rajadityashekhar@outlook.com>
2025-09-08 01:31:19 +0530

    Spring Security and Disable CSRF

---
+++ 25-spring-security/src/main/java/com/anjori/spring_security/basic/BasicAuthentication.java
@@ -0,0 +1,26 @@
+package com.anjori.spring_security.basic;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicAuthentication {
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+
+        http.sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS.STATELESS));
+
+        
+        http.httpBasic(customizer -> {});
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
+}
